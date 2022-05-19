49569266046c7c7779b89a8ef8e65176e58f18ae
Pavel <klon_008@mail.ru>
2022-05-19 11:13:23 +0700

    cookie

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var urlNinja = 'https://poe.ninja/api/data/currencyoverview?league=Sentinel&type=Currency';
 $(function() {
+    foo = $.cookie("scurwmdaAzxctilds");
+    if (foo){
+        $('#rate').val(foo);
+    }
     $("#exalt").numeric({ decimal : ".",  negative : false, scale: 3 });
+    $("#rate").numeric({ decimal : ".",  negative : false, scale: 3 });
     $("#exalt").on('change', ()=>{
-        $.ajax({
-            url: urlNinja,
-            xhrFields: {
-               withCredentials: true
-            }
-         }).done(function( data ) {
-            if ( console && console.log ) {
-              console.log( "Sample of data:", data );
-            }
-          });;
+        var res = 0;
+        var rate = $('#rate').val();
+        var exalt = $('#exalt').val();
+        res = rate * exalt;
+        $('#chaos').val(res);
     });
+    $('#rate').on('change', ()=>{
+        var rate =  $('#rate').val();
+        $.cookie('scurwmdaAzxctilds', rate);
+    })
 });
